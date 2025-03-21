77de5672acd69acc2c8853bd9e6dbd8e0cfe92f9
sonamgaur93 <sonamgaur93@gmail.com>
2025-03-21 17:01:00 +0530

    fix

--- src/main/java/com/poc/springboot_docker/Controller.java
+++ src/main/java/com/poc/springboot_docker/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class Controller {
 
-    @GetMapping("/hi")
+    @GetMapping
     public String get(){
         return "hello";
     }
